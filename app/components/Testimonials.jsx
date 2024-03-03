84c59a0aec6253c6bbee76976fd53e4fb8bfecaa
Swadhin Kumar Nayak <nayak.swadhin25@gmail.com>
2024-03-03 06:08:12 +0530

    class updated

--- app/components/Testimonials.jsx
+++ app/components/Testimonials.jsx
@@ -14,12 +14,12 @@ const items = [
             />
           </div>
           <h1 className="font-semibold mt-2">JHON DOE</h1>
-          <div class="flex items-center space-x-1 mt-2 justify-between">
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl ">&#9733;</span>
+          <div className="flex items-center space-x-1 mt-2 justify-between">
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl ">&#9733;</span>
           </div>
           <div className="text-[12px] text-[#dbdbd9]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
@@ -37,12 +37,12 @@ const items = [
             />
           </div>
           <h1 className="font-semibold mt-2">JHON DOE</h1>
-          <div class="flex items-center space-x-1 mt-2 justify-between">
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl ">&#9733;</span>
+          <div className="flex items-center space-x-1 mt-2 justify-between">
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl ">&#9733;</span>
           </div>
           <div className="text-[12px] text-[#dbdbd9]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
@@ -60,12 +60,12 @@ const items = [
             />
           </div>
           <h1 className="font-semibold mt-2">JHON DOE</h1>
-          <div class="flex items-center space-x-1 mt-2 justify-bewteen">
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl ">&#9733;</span>
+          <div className="flex items-center space-x-1 mt-2 justify-bewteen">
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl ">&#9733;</span>
           </div>
           <div className="text-[12px] text-[#dbdbd9]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
@@ -86,12 +86,12 @@ const items = [
             />
           </div>
           <h1 className="font-semibold mt-2">JHON DOE</h1>
-          <div class="flex items-center space-x-1 mt-2 justify-between">
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl ">&#9733;</span>
+          <div className="flex items-center space-x-1 mt-2 justify-between">
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl ">&#9733;</span>
           </div>
           <div className="text-[12px] text-[#dbdbd9]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
@@ -109,12 +109,12 @@ const items = [
             />
           </div>
           <h1 className="font-semibold mt-2">JHON DOE</h1>
-          <div class="flex items-center space-x-1 mt-2 justify-between">
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl ">&#9733;</span>
+          <div className="flex items-center space-x-1 mt-2 justify-between">
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl ">&#9733;</span>
           </div>
           <div className="text-[12px] text-[#dbdbd9]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
@@ -132,12 +132,12 @@ const items = [
             />
           </div>
           <h1 className="font-semibold mt-2">JHON DOE</h1>
-          <div class="flex items-center space-x-1 mt-2 justify-between">
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl text-[#a571ab]">&#9733;</span>
-            <span class="text-2xl ">&#9733;</span>
+          <div className="flex items-center space-x-1 mt-2 justify-between">
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl text-[#a571ab]">&#9733;</span>
+            <span className="text-2xl ">&#9733;</span>
           </div>
           <div className="text-[12px] text-[#dbdbd9]">
             <span></span>Lorem ipsum dolor sit amet consectetur adipisicing
